84a03fc00ace9401023f6c2172d9ce31fbccb488
José Carlos Valenzuela Nieto <valenzuelanietojosecarlos@gmail.com>
2023-11-18 21:38:32 +0100

    game_scene added

---
+++ game_scene.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var sprite3d: Sprite3D
+var timer: Timer
+
+func _ready():
+	sprite3d = $Pestaneo
+	timer = $Timer
+
+	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
+
+	timer.start()
+
+func _on_timer_timeout():
+	sprite3d.visible = true
+	var display_timer = Timer.new()
+	display_timer.wait_time = 2
+	display_timer.one_shot = true
+	add_child(display_timer)
+	display_timer.start()
+	display_timer.connect("timeout", Callable(self, "_on_display_timer_timeout"))
+
+	timer.start()
+
+func _on_display_timer_timeout():
+	sprite3d.visible = false
