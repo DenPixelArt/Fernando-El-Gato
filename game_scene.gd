813e23eb46a45ae7f1a0f0176c2783cfb6760412
DenPixelArt <denpixelart@gmail.com>
2023-11-19 10:33:02 +0100

    game_scene timers listos

--- game_scene.gd
+++ game_scene.gd
@@ -2,28 +2,32 @@ extends Node
 
 var sprite3d: Sprite3D
 var camera: Camera3D
-var timer: Timer
+var timerviendo: Timer
+var timerpestaneando: Timer
 
 func _ready():
 	sprite3d = $Pestaneo
-	timer = $TimerPestaneo
+	timerviendo = $TimerViendo
+	timerpestaneando = $TimerPestaneo
 	camera = $Marker3D/Camera3D
 	sprite3d.visible = true
-
-	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
 	
-	timer.start()    
-
-func _on_timer_timeout():
+func start_game():
+	sprite3d.visible = false
+	timerviendo.start()
+	
+func _on_timer_viendo_timeout():
 	sprite3d.visible = true
-	var display_timer = Timer.new()
-	display_timer.wait_time = 2
-	display_timer.one_shot = true
-	add_child(display_timer)
-	display_timer.start()
-	display_timer.connect("timeout", Callable(self, "_on_display_timer_timeout"))
+	timerpestaneando.start()
 
-	timer.start()
+func _on_timer_pestaneo_timeout():
+	sprite3d.visible = false
+	timerviendo.start()
 
 func _on_display_timer_timeout():
 	sprite3d.visible = false
+
+
+
+
+
