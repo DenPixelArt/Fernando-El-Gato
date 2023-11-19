8c4addaa38c2d8b2144db29cec0dc9c970ce2054
José Carlos Valenzuela Nieto <valenzuelanietojosecarlos@gmail.com>
2023-11-19 13:25:24 +0100

    TimeGame added

--- game_scene.gd
+++ game_scene.gd
@@ -4,11 +4,13 @@ var pestanas: Sprite3D
 var camarafotos
 var timerviendo: Timer
 var timerpestaneando: Timer
+var timergame: Timer 
 
 func _ready():
 	pestanas = $Pestaneo
 	timerviendo = $TimerViendo
 	timerpestaneando = $TimerPestaneo
+	timergame = $TimerGame
 	camarafotos = $habitacion/CamaraFotos
 	pestanas.visible = true
 	camarafotos.visible = false
@@ -17,6 +19,7 @@ func start_game():
 	pestanas.visible = false
 	camarafotos.visible = true
 	timerviendo.start()
+	timergame.start()
 	
 func _on_timer_viendo_timeout():
 	pestanas.visible = true
@@ -28,8 +31,5 @@ func _on_timer_pestaneo_timeout():
 	camarafotos.visible = true
 	timerviendo.start()
 
-
-
-
-
-
+func _on_timer_game_timeout():
+	get_tree().change_scene_to_file("res://ending.tscn") # Replace with function body.
