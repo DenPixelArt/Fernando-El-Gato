24279aa2e038fe2ec8c7e864963d05292834caa8
José Carlos Valenzuela Nieto <valenzuelanietojosecarlos@gmail.com>
2023-11-20 12:22:31 +0100

    Musica arreglada

--- Resultado.gd
+++ Resultado.gd
@@ -1,12 +1,7 @@
 extends Node
 
 var gatosfotografiados = []
-var Fernando
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+# Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
